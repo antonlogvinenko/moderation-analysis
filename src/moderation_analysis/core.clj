4fc263331a268ef5e29332c239e9877f386646b8
Anton Logvinenko <logvinenko@farpost.com>
2013-04-01 13:18:15 +1100

    master Initial commit

---
+++ src/moderation_analysis/core.clj
@@ -0,0 +1,1 @@
+(ns moderation-analysis.core)
---
+++ test/moderation_analysis/test/core.clj
@@ -0,0 +1,6 @@
+(ns moderation-analysis.test.core
+  (:use [moderation-analysis.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
